0d99b035f8557762e74fcf426aaa24ec91a71245
lijing-7 <1609071695@qq.com>
2019-09-06 17:23:36 +0800

    1.新增教师查询列表 2.新增积分消费记录明细查询 3.新增查询历史购买订单

--- src/main/java/com/hp/dao/UserDao.java
+++ src/main/java/com/hp/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.hp.dao;
 import java.util.List;
 import org.apache.ibatis.annotations.Select;
+
+import com.hp.bean.Credit;
 import com.hp.bean.User;
 
 public interface UserDao {
@@ -10,6 +12,14 @@ public interface UserDao {
 
 	@Select("select * from e_user where g_num = 2")
 	List<User> queryAllTeacher();
+
+	@Select("select * from e_credit where CREDIT_TEXT <> \"充值\"")
+	List<Credit> queryAllConsumption();
+	
+	@Select("select * from e_credit where CREDIT_TEXT =\"充值\"")
+	List<Credit> queryAllBuy();
+	
+	
 
 
 
--- src/main/java/com/hp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hp/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.hp.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hp.bean.Credit;
 import com.hp.bean.User;
 import com.hp.dao.UserDao;
 import com.hp.service.UserService;
@@ -22,6 +24,19 @@ public class UserServiceImpl implements UserService {
 		return userDao.queryAllTeacher();
 	}
 
+	public List<Credit> queryAllConsumption() {
+		// TODO Auto-generated method stub
+		return userDao.queryAllConsumption();
+	}
+
+	@Override
+	public List<Credit> queryAllBuy() {
+		// TODO Auto-generated method stub
+		return userDao.queryAllBuy();
+	}
+
+	
+	
 	
 
 }
